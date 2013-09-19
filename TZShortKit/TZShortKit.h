0c3d15123342738c740f5df46961675be2a5bde7
tadeuzagallo <tadeuzagallo@gmail.com>
2013-09-19 15:41:49 -0300

    target structure

---
+++ TZShortKit/TZShortKit.h
@@ -0,0 +1,14 @@
+//
+//  TZShortKit.h
+//  TZShortKit
+//
+//  Created by Tadeu Zagallo on 9/19/13.
+//  Copyright (c) 2013 Tadeu Zagallo. All rights reserved.
+//
+
+#ifndef TZShortKit_TZShortKit_h
+#define TZShortKit_TZShortKit_h
+
+
+
+#endif
