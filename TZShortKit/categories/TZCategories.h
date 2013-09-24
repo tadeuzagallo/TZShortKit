22ae5b34a86c047173af2cc49069aa71f8a520e1
tadeuzagallo <tadeuzagallo@gmail.com>
2013-09-24 12:53:53 -0300

    sub groups should have their own header

---
+++ TZShortKit/categories/TZCategories.h
@@ -0,0 +1,16 @@
+//
+//  TZCategories.h
+//  TZShortKit
+//
+//  Created by Tadeu Zagallo on 9/24/13.
+//  Copyright (c) 2013 Tadeu Zagallo. All rights reserved.
+//
+
+#ifndef TZShortKit_TZCategories_h
+#define TZShortKit_TZCategories_h
+
+#import "UIButton+TZShortKit.h"
+#import "UIImage+TZShortKit.h"
+#import "UIView+TZShortKit.h"
+
+#endif
